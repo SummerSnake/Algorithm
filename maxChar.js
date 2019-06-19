0e84a4aec7f1c2daf5ec112827cd5903f1784cac
summersnake <33661226lll@163.com>
2019-06-19 16:12:17 +0800

    【新增】字符串算法

---
+++ maxChar.js
@@ -0,0 +1,29 @@
+/**
+ * 统计一个字符串出现次数最多的字符。
+ */
+function findMaxDuplicateChar(str = '') {
+    if (str.length <= 1) {
+        return str;
+    }
+    let charObj = {};
+    Array.prototype.map.call(str, (item)=> {
+        if (!charObj[item]) {
+            charObj[item] = 1;
+        } else {
+            charObj[item] += 1;
+        }
+    });
+
+    let maxChar = '';
+    let maxVal = 1;
+    for (let key in charObj) {
+        if (charObj[key] >= maxVal) {
+            maxChar = key;
+            maxVal = charObj[key];
+        }
+    }
+    console.info(` 出现次数最多的字符：${maxChar}\r\n`, `出现的次数：${maxVal}`);
+}
+
+let str = 'afjghdfraaaasdenas';
+findMaxDuplicateChar(str);
