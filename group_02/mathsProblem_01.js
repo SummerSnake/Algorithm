1a8611b34cb0c65e605db84cb9978983ae45f5c8
liyan <33661226lll@163.com>
2019-10-15 16:53:55 +0800

    【新增】maths

--- group_02/mathsProblem_01.js
+++ group_02/mathsProblem_01.js
@@ -13,4 +13,25 @@ function calc() {
   console.log(`共经过${nowHeight}米，第10次反弹${nextHeight}米`);
 }
 
-calc();
+// calc();
+
+/**
+ * 一只猴子摘了N个桃子，第一天吃了一半又多吃了一个，
+ * 第二天又吃了余下的一半又多吃了一个，
+ * 到第十天的时候发现还有一个。
+ * 求一共有多少桃子。
+ */
+function calc2() {
+  let total = 0; // 桃子总数
+  let todayNum = 1; // 今天剩余的桃子（到第十天的时候发现还有一个）
+
+  for (let i = 9; i > 0; i -= 1) {
+    // 每天吃一半 +1，则 (todayNum + 1) * 2 即为前一天剩余的桃子
+    total = (todayNum + 1) * 2;
+    todayNum = total;
+    console.log(`第${i}天的桃子数为：${todayNum}；`);
+  }
+  console.log(`一共有${total}个桃子。`);
+}
+
+calc2();
