df4b14694ec62c300bb8e83ed57dd20d80df40bc
liyan <33661226lll@163.com>
2019-10-28 17:49:13 +0800

    feat: AVL树 -- 删除方法

--- 04/AVLTree.js
+++ 04/AVLTree.js
@@ -10,14 +10,12 @@
 /**
  * @desc 创建二叉树节点
  *
- * 1. 一个指向父亲节点的指针 parent
- * 2. 一个指向左节点的指针 left
- * 3. 一个指向右节点的指针 right
- * 4. 一个数据域，存放节点的值 val
+ * 1. 一个指向左节点的指针 left
+ * 2. 一个指向右节点的指针 right
+ * 3. 一个数据域，存放节点的值 val，及标识符 key
  */
 class AVLTreeNode {
   constructor(key, val) {
-    this.parent = null;
     this.left = null;
     this.right = null;
     this.key = key;
@@ -34,11 +32,7 @@ class AVLTree {
      * @param { object } element 当前节点
      */
     const getHeight = function (element) {
-      if (!element) {
-        return 0;
-      } else {
-        return Math.max(getHeight(element.left), getHeight(element.right)) + 1;
-      }
+      return !element ? 0 : Math.max(getHeight(element.left), getHeight(element.right)) + 1;
     };
 
     /**
@@ -102,7 +96,6 @@ class AVLTree {
       if (!node) {
         return null;
       }
-
       const leftHeight = getHeight(node.left);
       const rightHeight = getHeight(node.right);
       // 如果左右高度相等，则为平衡树，不做任何操作
@@ -110,16 +103,12 @@ class AVLTree {
         return node;
       }
       // 如果左子树高度比右子树高度大
-      if (leftHeight - rightHeight > 1) {
+      if (leftHeight - rightHeight >= 1) {
         // 如果左子树的左子树高度大于等于左子树的右子树高度，进行右单旋，否则需要先左后右双旋
         node = getHeight(node.left.left) >= getHeight(node.left.right) ? rotateRR(node) : rotateLR(node);
       } else { // 右子树高度比左子树高度大
-        // 如果右子树的右子树高度大于等于右子树的左子树高度, 进行左单旋，否则需要右左双旋
-        if (getHeight(node.right.right) >= getHeight(node.right.left)) {
-          node = rotateLL(node);
-        } else {
-          node = rotateRL(node);
-        }
+        // 如果右子树的右子树高度大于等于右子树的左子树高度, 进行左单旋，否则需要先右后左双旋转
+        node = getHeight(node.right.right) >= getHeight(node.right.left) ? rotateLL(node) : rotateRL(node);
       }
       return node;
     };
@@ -130,51 +119,117 @@ class AVLTree {
      * @param { object } newElement 要插入的节点
      */
     const _insert = function (element, newElement) {
+      let node = element;
       // 当前节点不存在，直接将新节点插入
-      if (!element) {
-        element = newElement;
-        return element;
+      if (!node) {
+        node = newElement;
+        return node;
       }
       // 当前节点值与新节点值相等，不做任何操作
-      if (newElement.val === element.val) {
-        return element;
+      if (newElement.val === node.val) {
+        return node;
       }
       // 如果新节点值小于当前节点，则插入到当前节点左子树
-      if (newElement.val < element.val) {
-        if (!element.left) { // 如果左子树为空，则直接插入
-          element.left = newElement;
+      if (newElement.val < node.val) {
+        if (!node.left) { // 如果左子树为空，则直接插入
+          node.left = newElement;
         } else { // 否则递归循环插入
-          element.left = _insert(element.left, newElement);
+          node.left = _insert(node.left, newElement);
           // 将树旋转至平衡
-          element = makeBalance(element);
+          node = makeBalance(node);
         }
       } else { // 插入右子树
-        if (!element.right) {
-          element.right = newElement;
+        if (!node.right) {
+          node.right = newElement;
         } else {
-          element.right = _insert(element.right, newElement);
-          element = makeBalance(element);
+          node.right = _insert(node.right, newElement);
+          node = makeBalance(node);
         }
       }
-      return element;
+      return node;
     };
     this.insert = function (key, val) {
       const newElement = new AVLTreeNode(key, val);
       this.root = _insert(this.root, newElement);
+    };
+
+    /**
+     * @desc 删除节点
+     * @param { object } element 当前节点
+     * @param { object } delElement 要删除的节点
+     */
+    const _delete = function (element, delElement) {
+      let node = element;
+      if (!node) {
+        return null;
+      }
+      // 如果要删除节点的val等于当前节点值，即是要删除的节点
+      if (delElement === node.val) {
+        // 如果是叶子节点，直接删除
+        if (!node.left && !node.right) {
+          return null;
+        }
+        // 如果有两个子节点
+        if (!!node.left && !!node.right) {
+          let temp = node.right;
+          // 查找右子树中最小的节点
+          while (temp['left'] !== null) {
+            temp = temp.left;
+          }
+          // 右子树中最小的节点赋值给当前节点
+          node.key = temp.key;
+          node.val = temp.val;
+          // 递归删除右子树中最小的节点
+          node.right = _delete(node.right, temp.val);
+          node = makeBalance(node);
+        } else { // 如果只有一个节点
+          node = !node.left ? node.right : node.left;
+        }
+        return node;
+      }
+      // 如果要删除节点的val小于根节点值，则从左子树查找
+      if (delElement < node.val) {
+        node.left = _delete(node.left, delElement);
+        node = makeBalance(node);
+      } else { // 如果要删除节点的val大于根节点值，则从右子树查找
+        node.right = _delete(node.right, delElement);
+        node = makeBalance(node);
+      }
+      return node;
+    };
+    this.delete = function (delElement) {
+      this.root = _delete(this.root, delElement);
+    };
+    /**
+     * @desc 打印树
+     */
+    this.print = function () {
+      console.log(this.root);
     }
   }
 }
 
 const tree = new AVLTree();
 const existData = [];
-for (let len = 10; len > 0; len--) {
+
+for (let len = 10; len > 0; len -= 1) {
   const data = ~~(100 * Math.random());
-  if (-1 === existData.indexOf(data)) {
+  if (!existData.includes(data)) {
     existData.push(data);
     tree.insert(data, data);
   } else {
-    len++;
+    len += 1;
   }
 }
 
-console.log(tree);
+tree.print();
+
+const deletedData = [];
+for (let len = 3; len > 0; len--) {
+  const index = ~~(Math.random() * existData.length);
+  deletedData.push(existData[index]);
+  tree.delete(existData.splice(index, 1));
+}
+
+console.warn(deletedData);
+tree.print();
