7af1e0dfd6963eef3ec564266123bc2d73e5349a
liyan <33661226lll@163.com>
2019-10-29 15:37:11 +0800

    feat: 数组查找算法 -- 插值查找

--- 02/search.js
+++ 02/search.js
@@ -45,14 +45,49 @@ class Search {
       let high = arr.length - 1;
 
       while (low <= high) {
-        mid = Math.ceil((high + low) / 2);
+        mid = Math.floor(low + (high - low) / 2);
         if (data === arr[mid]) {
           return mid;
         }
         if (data < arr[mid]) {
           high = mid - 1;
+        } else {
+          low = mid + 1;
         }
-        if (data > arr[mid]) {
+      }
+
+      return -1;
+    };
+
+    /**
+     * @desc 插值查找(要求数据为有序表)    时间复杂度O(logn)
+     * @param { array } arr 要查找的数据所属数组
+     * @param { any } data 要查找的数据
+     *
+     * 折半查找：mid=low+1/2*(high-low);
+     * 插值查找：mid=low+ (data-a[low])/(a[high]-a[low]) *(high-low);
+     *
+     * 也就是将折半查找的比例参数1/2改进为自适应的，根据关键字在整个有序表中所处的位置，
+     * 让mid值的变化更靠近要查找的值，这样也就间接地减少了比较次数。
+     * 对于表长较大，而关键字分布又比较均匀的查找表来说，插值查找算法的平均性能比折半查找要好的多。
+     * 反之，数组中如果分布非常不均匀，那么插值查找未必是很合适的选择。
+     */
+    this.insertSearch = (arr, data) => {
+      let low = 0;
+      let mid = 0;
+      let high = arr.length - 1;
+
+      while (low <= high) {
+        mid = Math.floor(low + (data - arr[low]) / (arr[high] - arr[low]) * (high - low));
+        if (mid < low || mid > high) { // 出现mid与上一次mid相同的情况跳出，防止死循环
+          break;
+        }
+        if (data === arr[mid]) {
+          return mid;
+        }
+        if (data < arr[mid]) {
+          high = mid - 1;
+        } else {
           low = mid + 1;
         }
       }
@@ -61,3 +96,9 @@ class Search {
     }
   }
 }
+
+
+const arr = [1, 19, 88, 109, 200, 230, 270, 299, 900, 1000, 1001, 5000, 5010, 5400,
+  5900, 7000, 8000, 8099, 9012, 9099, 9999, 10002, 12093, 15432];
+const search = new Search();
+console.log(search.insertSearch(arr, 9099));
