6b000a575d5bcb12e3c43ed092232e96d03df970
liyan <33661226lll@163.com>
2020-03-02 11:22:26 +0800

    feat: 扁平化数组，并去重排序

--- 03/diffItem.js
+++ 03/diffItem.js
@@ -19,7 +19,7 @@ diff(arr1, arr2);
  */
 function replaceWord(str, nowWord, newWord) {
   // 判断 nowWord 字符串首字母是否大写，若是，则将 newWord 字符串首字母也替换为大写；
-  if (nowWord.charAt(0) < "a") {
+  if (nowWord.charAt(0) < 'a') {
     let firstChar = newWord.charAt(0);
     newWord = newWord.replace(firstChar, firstChar.toUpperCase());
   }
@@ -27,5 +27,30 @@ function replaceWord(str, nowWord, newWord) {
   console.log(str);
 }
 
-replaceWord("Let us go to the store.", "store", "mall");
-replaceWord("His name is Tom.", "Tom", "john");
+replaceWord('Let us go to the store.', 'store', 'mall');
+replaceWord('His name is Tom.', 'Tom', 'john');
+
+/**
+ * 已知如下数组：
+ * let arr = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14] ] ] ], 10];
+ * 编写一个程序将数组扁平化去并除其中重复部分数据，最终得到一个升序且不重复的数组
+ */
+const formatArr = (arr = []) => {
+  if (Array.isArray(arr) && arr.length < 1) {
+    return arr;
+  }
+  // flat() node.js 不支持
+  // return Array.from(new Set(arr.flat(Infinity).sort((a, b) => a - b)));
+  return Array.from(
+    new Set(
+      arr
+        .join()
+        .split(',')
+        .map(Number)
+        .sort((a, b) => a - b)
+    )
+  );
+};
+
+let initArr = [[1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10];
+console.log(formatArr(initArr));
