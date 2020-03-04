8b20eba3f40c77ae34c0a539a6579db966a0a8d9
liyan <33661226lll@163.com>
2020-03-04 13:56:56 +0800

    feat: 递归求值

--- 03/fillArray.js
+++ 03/fillArray.js
@@ -1,31 +1,32 @@
 /**
  * @desc 完成一个函数，接受数组作为参数，数组元素为整数或者数组，数组元素包含整数或数组，函数返回扁平化后的数组
- * @param arr 要转换的数组
- * @param result 要得到的结果
+ * @param { array } arr 要转换的数组
+ * @param { array } result 每次递归扁平化后的数组
+ * @return { array } 扁平化后的数组
  */
-function flat(arr, result) {
-  let newArr = Array.isArray(result) ? result : [];
-
+const flat = (arr = [], result = []) => {
   arr.map(item => {
-    if (typeof item === "number") {
-      newArr.push(item);
+    if (typeof item === 'number') {
+      result.push(item);
     } else {
-      flat(item, newArr);
+      flat(item, result);
     }
   });
-  // console.log(newArr);
-}
+
+  return result;
+};
 
 let arr = [1, [2, [[3, 4], 5], 6]];
-flat(arr);
+console.log(flat(arr));
 
 /**
  * @desc 用100个随机整数对应的字符串填充数组
- * @param start 起始值
- * @param end 结束值
- * @param arr 要得到的结果
+ * @param { number } start 起始值
+ * @param { number } end 结束值
+ * @param { array } arr 要填充的数组
+ * @return { array } 填充完成的数组
  */
-function fillArray(start = 1, end = 100, arr) {
+const fillArray = (start = 1, end = 100, arr = []) => {
   let newArr = Array.isArray(arr) ? arr : [];
   let newEnd = end <= start ? start + 100 : end;
   let arrRange = newEnd - start;
@@ -33,30 +34,44 @@ function fillArray(start = 1, end = 100, arr) {
   for (let i = 0; i <= 100; i += 1) {
     newArr.push(`${Math.floor(Math.random() * arrRange) + start}`);
   }
-  // console.log(newArr);
-}
 
-fillArray(100, 1000, ["n"]);
+  return newArr;
+};
+
+console.log(fillArray(100, 1000, ['n']));
 
 /**
  * @desc 将类数组对象转换为数组
- * @param obj 要转换的类数组
+ * @param { object } obj 要转换的类数组
+ * @return { array } 转换完成的数组
  */
-function objTurnArr(obj = {}) {
+const objTurnArr = (obj = {}) => {
   let newArr = [];
-  if (Object.prototype.toString.call(obj) === "[object Object]" && Object.keys(obj).length > 0) {
+
+  if (Object.prototype.toString.call(obj) === '[object Object]' && Object.keys(obj).length > 0) {
     newArr = Array.prototype.slice.call(obj);
   }
-  console.log(newArr);
-}
+
+  return newArr;
+};
 
 // 有 length, 为类数组对象
 let obj = {
-  0: "China",
-  1: "ShanDong",
-  2: "QingDao",
-  3: "SummerSnake",
+  0: 'China',
+  1: 'ShanDong',
+  2: 'QingDao',
+  3: 'SummerSnake',
   length: 4
 };
 
-objTurnArr(obj);
+console.log(objTurnArr(obj));
+
+/**
+ * @desc 1100/1 + 1100/2 + 1100/3 + ... +1100/n = 10000 用递归方式，写出求n的代码
+ * @param { number } result 累加值，用于匹配边界
+ * @param { number } n +1递增
+ * @return { number } n的最终值
+ */
+const recursion = (result, n) => (result >= 10000 ? n - 1 : recursion(result + 1100 / n, n + 1));
+
+console.log(recursion(0, 1));
