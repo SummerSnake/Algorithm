3e905d851d589eb64419a280f2cb88eb300b3d25
liyan <33661226lll@163.com>
2020-03-11 17:40:55 +0800

    feat: 在既定时间做作业的学生人数

--- 03/fillArray.js
+++ 03/fillArray.js
@@ -5,7 +5,7 @@
  * @return { array } 扁平化后的数组
  */
 const flat = (arr = [], result = []) => {
-  arr.map(item => {
+  arr.map((item) => {
     if (typeof item === 'number') {
       result.push(item);
     } else {
@@ -61,7 +61,7 @@ let obj = {
   1: 'ShanDong',
   2: 'QingDao',
   3: 'SummerSnake',
-  length: 4
+  length: 4,
 };
 
 console.log(objTurnArr(obj));
@@ -75,3 +75,26 @@ console.log(objTurnArr(obj));
 const recursion = (result, n) => (result >= 10000 ? n - 1 : recursion(result + 1100 / n, n + 1));
 
 console.log(recursion(0, 1));
+
+/**
+ * @desc 给定两个整数数组 startTime（开始时间）和 endTime（结束时间），并指定一个整数 queryTime 作为查询时间。
+ *       已知，第 i 名学生在 startTime[i] 时开始写作业并于 endTime[i] 时完成作业。
+ *       请返回在查询时间 queryTime 时正在做作业的学生人数。
+ *       即返回能够使 queryTime 处于区间 [startTime[i], endTime[i]]（含）的学生人数。
+ */
+const busyStudent = (startTime, endTime, queryTime) => {
+  let ret = 0;
+
+  for (let i = 0; i < startTime.length; i++) {
+    if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
+      ret += 1;
+    }
+  }
+
+  return ret;
+};
+
+const startTime = [1, 2, 3];
+const endTime = [3, 2, 7];
+const queryTime = 4;
+console.log(busyStudent(startTime, endTime, queryTime));
--- 04/charEqual.js
+++ 04/charEqual.js
@@ -1,41 +1,48 @@
 /**
  * @desc 检查一个字符串(str)是否以指定的字符串(target)结尾。如果是，返回true;如果不是，返回false。
- *
- * substring() 方法用于提取字符串中介于两个指定下标之间的字符。substring(start,stop)；
- * substring() 方法返回的子串包括 start 处的字符，但不包括 stop 处的字符。
+ *       substring() 方法用于提取字符串中介于两个指定下标之间的字符。substring(start,stop)；
+ *       substring() 方法返回的子串包括 start 处的字符，但不包括 stop 处的字符。
+ * @param { string } str
+ * @param { string } target
+ * @return { boolean }
  */
-function confirmEnding(str, target) {
-  console.log(str.substring(str.length - target.length) === target);
-}
+const confirmEnding = (str, target) => str.substring(str.length - target.length) === target;
 
-confirmEnding("He has to give me a new name", "name");
+const str = 'He has to give me a new name';
+const target = 'name';
+console.log(confirmEnding(str, target));
 
 /**
  * @desc 重复一个指定的字符串 num 次，如果 num 是一个负数则返回一个空字符串。
+ * @param { string } str
+ * @param { number } num
+ * @return { string }
  */
-function repeatNum(str, num = 0) {
-  let originStr = "";
+const repeatNum = (str, num = 0) => {
+  let ret = '';
 
   if (num >= 0) {
     for (let i = 0; i < num; i += 1) {
-      originStr += str;
+      ret += str;
     }
   }
 
-  return originStr;
-}
+  return ret;
+};
 
-let str = "abcde";
-console.log(repeatNum(str, -1));
+let str1 = 'abcde';
+console.log(repeatNum(str1, 2));
 
 /**
  * @desc 把一个数组arr按照指定的数组大小size分割成若干个数组块。
- *
- * 说明：例如:chunk([1,2,3,4],2)=[[1,2],[3,4]];  chunk([1,2,3,4,5],2)=[[1,2],[3,4],[5]];
- * splice() 方法向/从数组中添加/删除项目，然后返回被删除的项目。
- * splice(index,howmany,item1,.....,itemX)，该方法会改变原始数组。
+ *       说明：例如:chunk([1,2,3,4],2)=[[1,2],[3,4]];  chunk([1,2,3,4,5],2)=[[1,2],[3,4],[5]];
+ *       splice() 方法向/从数组中添加/删除项目，然后返回被删除的项目。
+ *       splice(index,howmany,item1,.....,itemX)，该方法会改变原始数组。
+ * @param { any[] } arr
+ * @param { number } size
+ * @return { any[][] }
  */
-function arrChunk(arr = [], size = 0) {
+const arrChunk = (arr = [], size = 0) => {
   let newArrLen = Math.ceil(arr.length / size);
   let innerArr = [];
 
@@ -43,44 +50,51 @@ function arrChunk(arr = [], size = 0) {
     innerArr.push(arr.splice(0, size));
   }
 
-  console.log(innerArr);
-}
+  return innerArr;
+};
 
-arrChunk([1, 2, 3, 4, 5], 2);
+console.log(arrChunk([1, 2, 3, 4, 5], 2));
 
 /**
  * @desc 如果数组第一个字符串元素包含了第二个字符串元素的所有字符，函数返回true。
+ * @param { string[] } arr
+ * @return { boolean }
  */
-function charEqual(arr = []) {
+const charEqual = (arr = []) => {
   let isExist = false;
   let arrZero = arr[0].toLowerCase();
   let arrOne = arr[1].toLowerCase();
 
-  Array.prototype.forEach.call(arrOne, item => {
+  Array.prototype.forEach.call(arrOne, (item) => {
     isExist = arrZero.indexOf(item) !== -1;
   });
 
   console.log(isExist);
-}
+};
 
-charEqual(["Alien", "line"]);
+charEqual(['Alien', 'line']);
 
 /**
  * @desc 猜数字(查找两个数组中 下标与值都相等的数 的总个数)
- *
- * 输入：guess = [2,2,3], answer = [3,2,1]
- * 输出：1
+ *       输入：guess = [2,2,3], answer = [3,2,1]
+ *       输出：1
+ * @param { any[] } arr
+ * @return { any }
  */
-function guessGame(guess, answer) {
+const guessGame = (guess, answer) => {
   let i = 0;
   let num = 0;
-  while (i < 3) {
+  let len = Math.min(guess.length, answer.length);
+
+  while (i < len) {
     if (guess[i] === answer[i]) {
       num += 1;
     }
+
     i += 1;
   }
-  return num;
-}
 
-console.log(guessGame([2, 2, 3], [3, 2, 1]));
+  return num;
+};
+
+console.log(guessGame([3, 2, 3, 4], [3, 2, 1]));
