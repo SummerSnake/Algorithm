9c5950401af10d92dd7f36acfeaa1289a7fd35f6
liyan <33661226lll@163.com>
2020-06-26 14:21:08 +0800

    feat: 移除元素 => 首尾双指针

--- 09/removeElement.js
+++ 09/removeElement.js
@@ -6,13 +6,14 @@
  */
 
 /**
- * @desc 直接覆盖
+ * @desc 双指针 => 直接遍历覆盖
  * @param { any[] } arr
  * @param { any } val
  * @return { number }
  */
 const removeElement = (arr, val) => {
   let index = 0;
+
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== val) {
       arr[index] = arr[i];
@@ -26,12 +27,37 @@ const removeElement = (arr, val) => {
 console.log(removeElement([3, 2, 2, 3], 2));
 
 /**
- * @desc splice截取，指针减一
+ * @desc 首尾双指针 => 末尾项覆盖(当要删除的元素很少时，此种方式可以减少覆盖次数)
  * @param { any[] } arr
  * @param { any } val
  * @return { number }
  */
 const removeElement2 = (arr, val) => {
+  let index = 0;
+  let last = arr.length - 1;
+
+  while (index < last) {
+    if (arr[index] === val) {
+      // 取出最后一个元素进行覆盖
+      arr[index] = arr[last];
+      last--;
+    } else {
+      index++;
+    }
+  }
+
+  return index;
+};
+
+console.log(removeElement2([3, 2, 2, 3], 2));
+
+/**
+ * @desc splice截取，指针减一
+ * @param { any[] } arr
+ * @param { any } val
+ * @return { number }
+ */
+const removeElement3 = (arr, val) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === val) {
       arr.splice(i, 1);
@@ -42,4 +68,20 @@ const removeElement2 = (arr, val) => {
   return arr.length;
 };
 
-console.log(removeElement2([3, 2, 2, 3], 2));
+console.log(removeElement3([3, 2, 2, 3], 2));
+
+/**
+ * @desc 每次都定位出要删除的那个
+ * @param { any[] } arr
+ * @param { any } val
+ * @return { number }
+ */
+const removeElement4 = (arr, val) => {
+  while (arr.indexOf(val) > -1) {
+    arr.splice(arr.indexOf(val), 1);
+  }
+
+  return arr.length;
+};
+
+console.log(removeElement4([3, 2, 2, 3], 2));
