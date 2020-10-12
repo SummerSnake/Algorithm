9cb589c840c18219e1bee7b3ef22248483c598d0
liyan <33661226lll@163.com>
2020-10-12 15:40:54 +0800

    feat: 无重复的最长子串 => 滑动窗口

--- 11/lengthOfLongestSubstring.js
+++ 11/lengthOfLongestSubstring.js
@@ -34,3 +34,32 @@ const lengthOfLongestSubstring = (str) => {
 };
 
 console.log(lengthOfLongestSubstring(str));
+
+/**
+ * @desc 滑动窗口
+ * @param { string } str
+ * @return { number }
+ */
+const lengthOfLongestSubstring2 = (str) => {
+  let arr = []; //  使用一个数组来维护滑动窗口
+  let max = 0;
+
+  // 遍历字符串
+  for (let i = 0; i < str.length; i++) {
+    const index = arr.indexOf(str[i]);
+    // 判断字符是否在滑动窗口数组里，在则删除滑动窗口数组里相同字符及相同字符前的字符
+    if (index !== -1) {
+      arr.splice(0, index + 1);
+    }
+
+    // 将当前字符 push 进数组
+    arr.push(str.charAt(i));
+
+    // 然后将 max 更新为当前最长子串的长度
+    max = Math.max(arr.length, max);
+  }
+
+  return max;
+};
+
+console.log(lengthOfLongestSubstring2(str));
