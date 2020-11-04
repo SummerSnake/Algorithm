3393f156d09f958444dcdbcb7b59e382a5ad9c9b
liyan <33661226lll@163.com>
2020-11-04 16:54:56 +0800

    feat: 买卖股票的最佳时机 II => 动态规划

--- 12/maxProfit2.js
+++ 12/maxProfit2.js
@@ -16,7 +16,7 @@
 const prices = [7, 1, 5, 3, 6, 4];
 
 /**
- * @desc 暴力解法 (嵌套 for 循环)
+ * @desc 暴力解法 (枚举每一种买入和卖出的可能，最后得到最大值。)
  * @param { number[] } prices
  * @return { number }
  */
@@ -38,7 +38,7 @@ const maxProfit = (prices) => {
       for (let j = i + 1; j < prices.length; j++) {
         // 起始元素小于当前元素
         if (prices[i] < prices[j]) {
-          // 以起始元素为买入时间，当前元素为卖出时间，把所有可能的买卖组合都穷举一遍，比如1和5，3和6，计算总收益
+          // 以起始元素为买入时间，当前元素为卖出时间，把所有可能的买卖组合都枚举一遍，比如1和5，3和6，计算总收益
           let profit = calc(j + 1) + prices[j] - prices[i];
 
           if (profit > maxProfit) {
@@ -59,3 +59,78 @@ const maxProfit = (prices) => {
 };
 
 console.log(maxProfit(prices));
+
+/**
+ * @desc 动态规划 (矩阵)
+ *        矩阵即二维数组，两个维度分别是 天数(0, 1, ..., n-1) 和 是否持有股票(0 表不持有，1 表持有)；
+ *         1. 今天状态为未持有：
+ *            <1> 昨天未持有股票，今天不做任何操作；
+ *            <2> 昨天持有股票，今天卖出股票，获得利润；
+ *            <3> 转移方程：dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i])。
+ *         2. 今天状态为持有：
+ *            <1> 昨天持有股票，今天不做任何操作；
+ *            <2> 昨天未持有股票，今天买入股票，支出现金；
+ *            <3> 转移方程：dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i])。
+ *        边界状态：
+ *          第 i 天的状态由第 i-1 天状态推导而来，当 i=0 时；
+ *           <1> dp[0][0] = 0        # 第一天未持有股票，说明今天不做任何操作，获利为0；
+ *           <2> dp[0][1] = -prices[0]   # 第一天持有股票，说明今天买入股票，支出现金。
+ * @param { number[] } prices
+ * @return { number }
+ */
+const maxProfit2 = (prices) => {
+  const len = prices.length;
+  if (len < 2) {
+    return 0;
+  }
+
+  // 0：持有现金  1：持有股票    状态转移：0 → 1 → 0 → 1 → 0
+  // const dp = new Array(len).fill(new Array(2));
+  // Array.from 可以接受第二个参数，作用类似于数组的map方法，用来对每个元素进行处理，将处理后的值放入返回的数组，
+  // 传入的参数会覆盖第一个参数的值。
+  const dp = Array.from(new Array(len), () => new Array(2));
+
+  dp[0][0] = 0;
+  dp[0][1] = -prices[0];
+
+  for (let i = 1; i < len; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+  }
+
+  return dp[len - 1][0];
+};
+
+console.log(maxProfit2(prices));
+
+/**
+ * @desc 动态规划 (优化)
+ *        由于当前行只参考上一行，每一行就 2 个值，因此可以考虑使用「滚动变量」(「滚动数组」技巧)。
+ * @param { number[] } prices
+ * @return { number }
+ */
+const maxProfit3 = (prices) => {
+  const len = prices.length;
+  if (len < 2) {
+    return 0;
+  }
+
+  // cash：持有现金  hold：持有股票    状态转移：cash → hold → cash → hold → cash
+  let cash = 0;
+  let hold = -prices[0];
+
+  let preCash = cash;
+  let preHold = hold;
+
+  for (let i = 1; i < len; i++) {
+    cash = Math.max(preCash, preHold + prices[i]);
+    hold = Math.max(preHold, preCash - prices[i]);
+
+    preCash = cash;
+    preHold = hold;
+  }
+
+  return cash;
+};
+
+console.log(maxProfit3(prices));
