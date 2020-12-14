43117d06502874583358bba62e43ae6ce620252b
liyan <33661226lll@163.com>
2020-12-14 11:13:44 +0800

    feat: 二维数组中的查找 => 暴力解法

---
+++ 13/findNumInMatrix.js
@@ -0,0 +1,35 @@
+/**
+ * 二维数组中的查找 (剑指 offer 04)
+ *
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 要求完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ */
+
+const matrix = [
+  [1, 4, 7, 11, 15],
+  [2, 5, 8, 12, 19],
+  [3, 6, 9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30],
+];
+const target = 20;
+
+/**
+ * @desc 暴力解法
+ * @param { number[] } matrix
+ * @param { number } target
+ * @return { number }
+ */
+const findNumInMatrix = (matrix, target) => {
+  for (let i = 0; i < matrix.length; i++) {
+    if (target >= matrix[i][0] && target <= matrix[i][matrix[i].length - 1]) {
+      if (matrix[i].includes(target)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+console.log(findNumInMatrix(matrix, target));
