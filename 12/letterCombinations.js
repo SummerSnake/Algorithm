20f31178a61fe78df0103861b19cffe86f238c1c
liyan <33661226lll@163.com>
2020-12-15 17:43:44 +0800

    feat: 电话号码字母组合 => 深度优先遍历

--- 12/letterCombinations.js
+++ 12/letterCombinations.js
@@ -1,11 +1,11 @@
 /**
  * 电话号码的字母组合 (力扣 17)
  *
- * 给定一个仅包含数字的字符串，返回所有它能表示的字母组合。
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
  * 给出数字到字母的映射 json (与电话按键相同)。
  */
 
-const digits = '10086';
+const digits = '78';
 const json = {
   2: 'abc',
   3: 'def',
@@ -18,11 +18,41 @@ const json = {
 };
 
 /**
- * @desc BFS (广度优先搜索)
+ * @desc DFS (深度优先搜索)
  * @param { string } digits
  * @return { string[] }
  */
 const letterCombinations = (digits) => {
+  if (!digits || !digits.length) {
+    return [];
+  }
+
+  const res = [];
+  const dfs = (currStr, i) => {
+    if (i >= digits.length) {
+      res.push(currStr);
+      return;
+    }
+
+    letters = json[digits[i]];
+    for (let item of letters) {
+      dfs(currStr + item, i + 1);
+    }
+  };
+
+  dfs('', 0);
+
+  return res;
+};
+
+console.log(letterCombinations(digits));
+
+/**
+ * @desc BFS (广度优先搜索)
+ * @param { string } digits
+ * @return { string[] }
+ */
+const letterCombinations2 = (digits) => {
   if (!digits || !digits.length) {
     return [];
   }
@@ -37,9 +67,9 @@ const letterCombinations = (digits) => {
 
     for (let j = 0; j < len; j++) {
       const char = queue.shift();
-      const str = json[digits[i]];
+      const letters = json[digits[i]];
 
-      for (const item of str) {
+      for (const item of letters) {
         queue.push(char + item);
       }
     }
@@ -48,4 +78,4 @@ const letterCombinations = (digits) => {
   return queue;
 };
 
-console.log(letterCombinations(digits));
+console.log(letterCombinations2(digits));
