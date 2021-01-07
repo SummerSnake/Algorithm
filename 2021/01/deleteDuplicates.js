8a8e6941dd66287a11b9f9178c91fad900a068e6
liyan <33661226lll@163.com>
2021-01-07 15:40:46 +0800

    feat: 删除排序链表中的重复元素 => 原地删除

--- 2021/01/deleteDuplicates.js
+++ 2021/01/deleteDuplicates.js
@@ -50,3 +50,24 @@ const deleteDuplicates = (head) => {
 };
 
 deleteDuplicates(linkedList);
+
+/**
+ * @desc 原地删除
+ * @param { ListNode } head
+ * @return { ListNode }
+ */
+const deleteDuplicates2 = (head) => {
+  let node = head;
+
+  while (node && node.next) {
+    if (node.val === node.next.val) {
+      node.next = node.next.next;
+    } else {
+      node = node.next;
+    }
+  }
+
+  return head;
+};
+
+deleteDuplicates2(linkedList);
