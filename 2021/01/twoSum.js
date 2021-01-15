7cdad0fbd358ebea940008cf1449fccb424cc559
liyan <33661226lll@163.com>
2021-01-15 15:40:21 +0800

    feat: 两数之和 II - 输入有序数组 => 双指针

---
+++ 2021/01/twoSum.js
@@ -0,0 +1,39 @@
+/**
+ * 两数之和 II - 输入有序数组 (力扣 167)
+ *
+ * 给定一个已按照 升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ * 返回的下标值从 1 开始。
+ */
+
+const numbers = [2, 7, 11, 15];
+const target = 9;
+
+/**
+ * @desc 双指针
+ * @param { number[] } numbers
+ * @param { number } target
+ * @return { number[] }
+ */
+const twoSum = (numbers, target) => {
+  let i = 0;
+  let j = numbers.length - 1;
+
+  while (i < j) {
+    const sum = numbers[i] + numbers[j];
+
+    if (sum === target) {
+      return [i + 1, j + 1];
+    }
+    if (sum > target) {
+      j--;
+    }
+    if (sum < target) {
+      i++;
+    }
+  }
+
+  return [];
+};
+
+twoSum(numbers, target);
