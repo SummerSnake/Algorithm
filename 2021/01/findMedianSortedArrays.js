42df49062e3c7427f9218a6858e3ea6a6be1e398
liyan <33661226lll@163.com>
2021-01-28 11:03:51 +0800

    feat: 寻找两个正序数组的中位数 => 归并排序

--- 2021/01/findMedianSortedArrays.js
+++ 2021/01/findMedianSortedArrays.js
@@ -22,3 +22,40 @@ const findMedianSortedArrays = (nums1, nums2) => {
 };
 
 findMedianSortedArrays(nums1, nums2);
+
+/**
+ * @desc 归并排序
+ * @param { number[] } nums1
+ * @param { number[] } nums2
+ * @return { number }
+ */
+const findMedianSortedArrays2 = (nums1, nums2) => {
+  const len = nums1.length + nums2.length;
+  const arr = new Array(len);
+
+  let i = 0;
+  let j = 0;
+  let index = 0;
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] < nums2[j]) {
+      arr[index++] = nums1[i++];
+    } else {
+      arr[index++] = nums2[j++];
+    }
+  }
+
+  // 处理剩余元素
+  while (i < nums1.length) {
+    arr[index++] = nums1[i++];
+  }
+  while (j < nums1.length) {
+    arr[index++] = nums2[j++];
+  }
+
+  // 取中间点
+  const mid = Math.floor(arr.length / 2);
+
+  return arr.length % 2 === 0 ? (arr[mid - 1] + arr[mid]) / 2 : arr[mid];
+};
+
+findMedianSortedArrays2(nums1, nums2);
