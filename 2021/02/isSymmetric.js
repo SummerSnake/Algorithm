86c9ded3805805001ca9011d748bea9ddbe1f863
liyan <33661226lll@163.com>
2021-02-18 17:09:27 +0800

    feat: 对称二叉树 => 迭代

--- 2021/02/isSymmetric.js
+++ 2021/02/isSymmetric.js
@@ -63,3 +63,40 @@ const isSymmetric = (root) => {
 };
 
 isSymmetric(binaryTree);
+
+/**
+ * @desc 迭代
+ * @param { TreeNode } root
+ * @return { boolean }
+ */
+const isSymmetric2 = (root) => {
+  if (!root) {
+    return true;
+  }
+
+  const queue = [root.left, root.right];
+
+  while (queue.length > 0) {
+    const left = queue.shift();
+    const right = queue.shift();
+
+    if (!left && !right) {
+      continue;
+    }
+
+    if (!left || !right) {
+      return false;
+    }
+
+    if (left.val !== right.val) {
+      return false;
+    }
+
+    queue.push(left.left, right.right);
+    queue.push(left.right, right.left);
+  }
+
+  return true;
+};
+
+isSymmetric2(binaryTree);
