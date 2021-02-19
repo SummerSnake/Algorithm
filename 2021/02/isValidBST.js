2263e09902611b615ad3f70de4d645ab8911b234
liyan <33661226lll@163.com>
2021-02-19 17:03:57 +0800

    feat: 验证二叉搜索树 => 中序遍历(递归)

--- 2021/02/isValidBST.js
+++ 2021/02/isValidBST.js
@@ -54,3 +54,33 @@ const isValidBST = (root) => {
 };
 
 isValidBST(binaryTree);
+
+/**
+ * @desc 中序遍历(递归)
+ * @param { TreeNode } root
+ * @return { boolean }
+ */
+const isValidBST2 = (root) => {
+  let prev = -Infinity;
+
+  const inOrder = (node) => {
+    if (!node) {
+      return true;
+    }
+
+    // 中序遍历输出的值有序 2,3,4,5,6,7,8；
+    // 所以我们保存上一个节点值 prev，与当前节点值作比较；
+    // 如果当前值小于等于上一个值，那么这个树不是二叉搜索树。
+    if (inOrder(node.left) && prev < node.val) {
+      prev = node.val;
+
+      return inOrder(node.right);
+    }
+
+    return false;
+  };
+
+  return inOrder(root);
+};
+
+isValidBST2(binaryTree);
