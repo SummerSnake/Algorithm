cbd61d2148e68236ef7e49454f9c111576b009e3
liyan <33661226lll@163.com>
2021-03-15 15:56:20 +0800

    feat: 组合总和 => 回溯 + 剪枝

--- 2021/02/combinationSum.js
+++ 2021/02/combinationSum.js
@@ -37,3 +37,39 @@ const combinationSum = (candidates, target) => {
 };
 
 combinationSum([1, 2], 4);
+
+/**
+ * @desc 回溯 + 剪枝
+ * @param { number[] } candidates
+ * @param { number } target
+ * @return { number[][] }
+ */
+const combinationSum2 = (candidates, target) => {
+  const res = [];
+
+  candidates.sort((a, b) => a - b);
+  const dfs = (arr, curr, index) => {
+    // 符合条件的组合
+    if (curr === 0) {
+      res.push(arr);
+      return;
+    }
+
+    for (let i = index; i < candidates.length; i++) {
+      // candidates = [1,3,5,6]; target = 8;
+      // 当 arr = [1, 3] 时：target = 8 - 1 - 3 = 4，此时 4 < 5，不符合条件；
+      // 同理之后比5更大的元素也是不符合条件的，对于所有组合中以[1,3]为首的组合无需再进行下一步组合，直接进行下一轮组合
+      if (curr < candidates[i]) {
+        break;
+      }
+
+      // 每个元素可以被无限重复选取，所以以当前元素为搜索下标
+      dfs([...arr, candidates[i]], curr - candidates[i], i);
+    }
+  };
+
+  dfs([], target, 0);
+  return res;
+};
+
+combinationSum2([1, 2], 4);
