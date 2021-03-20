9ed54c5f45e4ecfbbe29dabd7bef8205072c84fd
summersnake <33661226lll@163.com>
2021-03-20 17:50:40 +0800

    feat: 逆波兰表达式 => 数组模拟栈

--- 2021/02/evalRPN.js
+++ 2021/02/evalRPN.js
@@ -14,15 +14,14 @@
 const evalRPN = tokens => {
   const stack = [];
 
-  for (let item of tokens) {
+  for (const item of tokens) {
     if (!isNaN(parseInt(item))) {
-      stack.push(~~item);
+      stack.push(item);
       continue;
     }
 
-    let y = stack.pop();
-    let x = stack.pop();
-
+    const y = stack.pop();
+    const x = stack.pop();
     switch (item) {
       case '+':
         stack.push(x + y);
@@ -44,3 +43,50 @@ const evalRPN = tokens => {
 };
 
 evalRPN(['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+']);
+
+/**
+ * @desc 数组模拟栈
+ * @param { string[] } tokens
+ * @return { number }
+ */
+const evalRPN2 = tokens => {
+  // 对于一个有效的逆波兰表达式，其长度 n 一定是奇数，且操作数的个数一定比运算符的个数多 1 个
+  const stack = new Int32Array(Math.floor((tokens.length + 1) / 2));
+  let index = -1;
+
+  for (const item of tokens) {
+    if (!isNaN(parseInt(item))) {
+      stack[index + 1] = item;
+      index++;
+      continue;
+    }
+
+    // 栈内处理运算
+    switch (item) {
+      case '+':
+        index--;
+        stack[index] += stack[index + 1];
+        break;
+      case '-':
+        index--;
+        stack[index] -= stack[index + 1];
+        break;
+      case '*':
+        index--;
+        stack[index] *= stack[index + 1];
+        break;
+      case '/':
+        index--;
+        stack[index] =
+          stack[index] / stack[index + 1] > 0
+            ? Math.floor(stack[index] / stack[index + 1])
+            : Math.ceil(stack[index] / stack[index + 1]);
+        break;
+      default:
+    }
+  }
+
+  return stack[index];
+};
+
+evalRPN2(['0', '3', '/']);
