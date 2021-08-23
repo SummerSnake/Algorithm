6f686e2551f6648797d6c1afce4ab9b020be885b
liyan <33661226lll@163.com>
2021-08-23 17:18:32 +0800

    feat: 整数转罗马数字 => 模拟

--- 2021/03/intToRoman.js
+++ 2021/03/intToRoman.js
@@ -23,3 +23,41 @@ const intToRoman = (num) => {
 };
 
 intToRoman(10);
+
+/**
+ * @desc 模拟
+ * @param { number } num
+ * @return { number }
+ */
+const intToRoman2 = (num) => {
+  const valueSymbols = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I'],
+  ];
+  const roman = [];
+
+  for (const [value, symbol] of valueSymbols) {
+    while (num >= value) {
+      num -= value;
+      roman.push(symbol);
+    }
+    if (num === 0) {
+      break;
+    }
+  }
+
+  return roman.join('');
+};
+
+intToRoman2(10);
