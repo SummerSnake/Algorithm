4e70fb15c548a2be90149146dc0e82f0a50db3f6
liyan <33661226lll@163.com>
2021-08-28 16:33:06 +0800

    feat: 数据流的中位数 => 大顶堆 & 小顶堆

--- 2021/03/MedianFinder.js
+++ 2021/03/MedianFinder.js
@@ -1,6 +1,7 @@
 /**
  * 数据流的中位数 (力扣 295)
  */
+const Heap = require('../04/Heap');
 
 /**
  * @desc 二分法
@@ -84,3 +85,60 @@ obj.addNum(4);
 obj.addNum(5);
 
 obj.findMedian();
+
+/**
+ * @desc 大顶堆 & 小顶堆
+ */
+class MedianFinder2 {
+  constructor() {
+    this.size = 0;
+    this.minHeap = new Heap('min'); // 小顶堆
+    this.maxHeap = new Heap('max'); // 大顶堆
+  }
+
+  /**
+   * @desc 添加元素
+   *        1. 插入前两者元素个数相等，说明插入前数据流元素个数为偶数，插入后变为奇数；
+   *          <1> 将元素插入到 minHeap 中，取出 minHeap 中的堆顶值插入到 maxHeap 中，此操作将保证
+   *              maxHeap 元素个数大于等于 minHeap 元素个数。
+   *        2. 插入前两者元素个数不相等，说明插入前数据流元素个数为奇数，插入后变为偶数；
+   *          <2> maxHeap 元素个数大于 minHeap 元素个数，将元素插入到 maxHeap 中，为保证两者元素个数相等
+   *              取出 maxHeap 中的堆顶值插入到 minHeap 中。
+   * @param { number } num
+   * @return { void }
+   */
+  addNum(num) {
+    this.size++;
+
+    if (this.maxHeap.size() === this.minHeap.size()) {
+      this.minHeap.insert(num);
+      this.maxHeap.insert(this.minHeap.pop());
+    } else {
+      this.maxHeap.insert(num);
+      this.minHeap.insert(this.maxHeap.pop());
+    }
+  }
+
+  /**
+   * @desc 获取中位数
+   * @return { number }
+   */
+  findMedian() {
+    // 奇数直接返回大顶堆堆顶元素，偶数计算中位数
+    if (this.size % 2 !== 0) {
+      return this.maxHeap.peak();
+    } else {
+      return (this.maxHeap.peak() + this.minHeap.peak()) / 2;
+    }
+  }
+}
+
+const obj2 = new MedianFinder2();
+obj2.addNum(1);
+obj2.addNum(2);
+obj2.addNum(2);
+obj2.addNum(3);
+obj2.addNum(4);
+obj2.addNum(5);
+
+obj2.findMedian();
