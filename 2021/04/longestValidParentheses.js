4a1284196b9ad27ef74d80396ca63e0909ad021b
liyan <33661226lll@163.com>
2021-12-08 16:39:48 +0800

    feat: 最长有效括号 => 滑动窗口

--- 2021/04/longestValidParentheses.js
+++ 2021/04/longestValidParentheses.js
@@ -42,3 +42,49 @@ const longestValidParentheses = (s) => {
 };
 
 longestValidParentheses(s);
+
+/**
+ * @desc 滑动窗口
+ * @param { string } s
+ * @return { number }
+ */
+const longestValidParentheses2 = (s) => {
+  // 向左右两边不断扩大滑动窗口
+  const expand = (left, right) => {
+    while (s[left - 1] === '(' && s[right + 1] === ')') {
+      left--;
+      right++;
+    }
+
+    return [left, right];
+  };
+
+  const map = new Map();
+  let left = s.indexOf('()', 0); // 以第一个 '()' 为中心
+  let right = 0;
+  let max = 0;
+
+  while (left > -1) {
+    left = s.indexOf('()', right);
+    right = left + 1;
+
+    // 向左右两边不断扩大滑动窗口
+    [left, right] = expand(left, right);
+
+    // 当窗口扩大到最大时，如果当前窗口的左边界刚好挨着前一个窗口的右边界，
+    // 则将这两个窗口合并，再以这个新合并的窗口为中心，向两侧扩大滑动窗口
+    while (map.get(left - 1)) {
+      [left, right] = expand(map.get(left - 1), right);
+    }
+
+    // 记录当前窗口的左右边界，key 是窗口右边界，value 是窗口左边界
+    map.set(right, left);
+
+    // 更新最大窗口
+    max = Math.max(max, right - left + 1);
+  }
+
+  return max;
+};
+
+longestValidParentheses2(s);
